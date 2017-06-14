4fd4eb0febc5137dc77947f251bedb398407e536
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-06-14 22:31:11 +0200

    Initial Commit

---
+++ Cocktail Kit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cocktail Kit
+//
+//  Created by Thibault Deutsch on 14/06/2017.
+//  Copyright © 2017 ATKF. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
