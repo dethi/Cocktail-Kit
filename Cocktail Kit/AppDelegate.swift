04ed890eac70e1e3e0460488b8130c84c9ee4506
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-06-15 16:22:55 +0200

    design: define global tint color

--- Cocktail Kit/AppDelegate.swift
+++ Cocktail Kit/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let themeColor = UIColor(red: 0.961, green: 0.580, blue: 0.000, alpha: 1.00)
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window?.tintColor = themeColor
         return true
     }
 
