8cfbfd671bd7767af7f443e48a05cbe313f0c36b
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-06-28 18:50:47 +0200

    data: don't keep record without image

--- Scripts/index.py
+++ Scripts/index.py
@@ -27,22 +27,33 @@ def mapping(data):
     for i in range(1, 16):
         ingredient = d['strIngredient{}'.format(i)]
         measure = d['strMeasure{}'.format(i)]
-        image = '{}/{}-Medium.png'.format(INGREDIENT_IMAGE_URL, ingredient)
+        image = '{}/{}-Small.png'.format(INGREDIENT_IMAGE_URL, ingredient)
 
         if ingredient:
             ingredients.append({
-                'name': ingredient,
-                'measure': measure,
+                'name': ingredient.strip(),
+                'measure': measure.strip(),
                 'image': image
             })
 
-    new['ingredients']= ingredients
+    new['ingredients'] = ingredients
     return new
+
+
+def should_keep(r):
+    return all(r[field] for field in ['name', 'instructions', 'image'])
 
 
 def main():
     if len(sys.argv) != 2:
         print('usage: ./index.py [PATHNAME]')
+        sys.exit(1)
+
+    app_id = os.getenv('ALGOLIA_APPLICATION_ID')
+    api_key = os.getenv('ALGOLIA_API_KEY')
+
+    if not (app_id and api_key):
+        print('Please define ALGOLIA_APPLICATION_ID and ALGOLIA_API_KEY env.')
         sys.exit(1)
 
     batch = []
@@ -56,11 +67,15 @@ def main():
             data = json.load(f)
 
         r = mapping(data)
-        batch.append(r)
+        if should_keep(r):
+            batch.append(r)
 
-    client = algoliasearch.Client("QJD6ORETUC",
-                                  "XXX_CHANGE_ME")
+    for i, r in enumerate(batch):
+        r['objectID'] = i
+
+    client = algoliasearch.Client(app_id, api_key)
     index = client.init_index("Cocktail-Kit")
+    index.clear_index()
     index.add_objects(batch)
 
 
